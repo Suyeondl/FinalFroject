cc4304710442fc558a601ecc52c57d56f423deac
Suyeondl <suyeon2355@gmail.com>
2023-04-11 03:23:50 +0900

    UI 변경

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,8 +1,56 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { Text, View, TextInput, TouchableOpacity, ImageBackground } from "react-native";
+import { styles } from "../style";
 
-const Home = () => {
+const Home = (props) => {
   return(
-    <Text>hello Home!</Text>
+    <ImageBackground>
+    <View style = {styles.mainView}>
+
+    {/* Info 버튼 */}
+    <TouchableOpacity 
+      style = {styles.infoBTN}
+      onPress={() => {
+        props.navigation.navigate("Info")
+      }}>
+        <Text style = {styles.homeText}>(관리자 정보)</Text>
+    </TouchableOpacity>
+
+    {/* Station, 고객센터 버튼 */}
+    <TouchableOpacity 
+      style = {styles.homeBTN}
+      onPress={() => {
+        props.navigation.navigate("Station")
+      }}>
+        <Text style = {styles.homeText}>Station</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style = {styles.homeBTN4}
+      onPress={() => {
+        props.navigation.navigate("Service")
+      }}>
+        <Text style = {styles.homeText}>고객센터</Text>
+    </TouchableOpacity>
+
+    {/* User, 폐우산 버튼 */}
+    <TouchableOpacity 
+      style = {styles.homeBTN2}
+      onPress={() => {
+        props.navigation.navigate("User")
+      }}>
+        <Text style = {styles.homeText}>User</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style = {styles.homeBTN3}
+      onPress={() => {
+        props.navigation.navigate("Umbrella")
+      }}>
+        <Text style = {styles.homeText}>폐우산</Text>
+    </TouchableOpacity>
+
+    </View>
+    </ImageBackground>
+
   )
 }
 export default Home;
--- Screens/Login.js
+++ Screens/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Text, View, TextInput, TouchableOpacity, ImageBackground } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, ImageBackground, Image } from "react-native";
 import { event } from "react-native-reanimated";
+import { styles } from "../style";
 
 const Login = (props) => {
     const [idTextInput, setIdTextinput] = useState("") //입력받은 id
@@ -9,7 +10,6 @@ const Login = (props) => {
 
     //로그인 입력값 useState에 저장
     const idChangeInput = (event) => {
-      set
       setIdTextinput(event)
     }
     const pwChangeInput = (event) => {
@@ -17,19 +17,35 @@ const Login = (props) => {
     }
 
   return(
-    <ImageBackground>
-    <View>
-
+    <ImageBackground style={styles.image} source={require('../images/LoginScreen.png')} resizeMode='cover'>
+    <View style = {styles.mainView}>
+    
     <TextInput
+      style = {styles.loginputText}
       value = {idTextInput}
       onChangeText = {idChangeInput}
       placeholder="ID"
     />
     <TextInput
+      style = {styles.loginputText}
       value = {pwTextInput}
       onChangeText = {pwChangeInput}
       placeholder="PASSWORD"
     />
+    <TouchableOpacity
+      style = {styles.loginBTN}
+      onPress={() => {
+        props.navigation.navigate("Home")
+      }}>
+        <Text style = {styles.loginText}>Login</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style = {styles.loginBTN}
+      onPress={() => {
+        props.navigation.navigate("SignUp")
+      }}>
+        <Text style = {styles.loginText}>SignUp</Text>
+    </TouchableOpacity>
 
     </View>
     </ImageBackground>
