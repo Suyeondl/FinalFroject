8382e4a1731dd45a63325f5493579c96a573c23e
Suyeondl <suyeon2355@gmail.com>
2023-04-16 22:45:11 +0900

    library 설치

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,13 +8,15 @@ import "firebase/compat/storage";
 
 // Initialize Firebase
 //파이어베이스 사이트에서 봤던 연결정보를 여기에 가져옵니다
+//임시 firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB_h8fvb2J3XFdP6TGlynRsNV_79fxlZvU",
-  authDomain: "finalproject-9e9c9.firebaseapp.com",
-  projectId: "finalproject-9e9c9",
-  storageBucket: "finalproject-9e9c9.appspot.com",
-  messagingSenderId: "111093244594",
-  appId: "1:111093244594:web:6f138df7685f0f61abdfab"
+  apiKey: "AIzaSyBb5gReEKJitLYdD-zjiYMtk91f35gmpwc",
+  authDomain: "teamproject-4c0d8.firebaseapp.com",
+  projectId: "teamproject-4c0d8",
+  storageBucket: "teamproject-4c0d8.appspot.com",
+  messagingSenderId: "1090193024503",
+  appId: "1:1090193024503:web:ae0088fe5d97a251c13af2",
+  measurementId: "G-5DEQ9PBBDR"
 };
 
 //사용 방법입니다.
