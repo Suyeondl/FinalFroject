aec9e8e0724d6c3353f9671c8302da45645492cc
Suyeondl <suyeon2355@gmail.com>
2023-04-16 22:45:20 +0900

    스크린 추가

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Screens/Login';
 import Home from './Screens/Home';
 import SignUp from './Screens/SignUp';
-import Service from './Screens/Service';
-import Station from './Screens/Station';
-import Umbrella from './Screens/Umbrella';
-import User from './Screens/User';
-import Info from './Screens/Info';
+import Service from './Screens/ServiceScreens/Service';
+import Station from './Screens/StationScreens/Station';
+import Umbrella from './Screens/UmbrellaScreens/Umbrella';
+import User from './Screens/UserScreens/User';
+import Info from './Screens/InfoScreens/Info';
+import RentalReturnReport from './Screens/ServiceScreens/RentalReturnReport';
+import GraySmallButton from './Screens/ServiceScreens/GraySmallButton';
+import FaultReport from './Screens/ServiceScreens/FaultReport';
 
 //Stack Navigation함수를 Stack변수명으로 저장
 const Stack = createStackNavigator();
@@ -22,7 +25,7 @@ export default function App() {
           headerTintColor: '#6699FF',
           headerTitleStyle: {fontWeight: "bold", fontSize:23},
         }}>
-
+ 
       <Stack.Screen
         name = "Login"
         component = {Login}
@@ -70,7 +73,26 @@ export default function App() {
         component = {Info}
         options = {{title : "내 정보"}}
       />
+
+      <Stack.Screen
+        name = "RentalReturnReport"
+        component = {RentalReturnReport}
+        options = {{title : "내 정보"}}
+      />
+
+      <Stack.Screen
+        name = "FaultReport"
+        component = {FaultReport}
+        options = {{title : "내 정보"}}
+      />
+
+      <Stack.Screen
+        name = "GraySmallButton"
+        component = {GraySmallButton}
+        options = {{title : "내 정보"}}
+      />
       
+
       </Stack.Navigator>
     </NavigationContainer>
   );
