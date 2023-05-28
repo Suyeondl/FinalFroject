756d30f0faed6ed97ebfb05b052e9abd4a05fe31
Suyeondl <suyeon2355@gmail.com>
2023-05-28 11:04:57 +0900

    Station 정보 확인

--- Screens/StationScreens/Station.js
+++ Screens/StationScreens/Station.js
@@ -1,36 +1,45 @@
-import { useState } from "react";
-import { Text, View, TextInput, TouchableOpacity, ImageBackground } from "react-native";
-import { styles } from "../../style";
-//C:\FinalProject\node_modules\react-native\ReactAndroid\build.gradle
+import React, { useEffect, useState } from 'react';
+import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
+import { styles } from '../../style';
 
-
-/* Station 정보 관리 - 대여가능여부, 위치 및 주소, 대여 우산 여부, 폐우산 적재 갯수 확인 가능 및 수정, 삭제, 등록 */
 const Station = (props) => {
-  return(
-    <ImageBackground style={styles.image} source={require('../../images/MainScreen.png')} resizeMode="cover">
-    <View style = {styles.mainView}>
+  const [stations, setStation] = useState([]);
 
-    <TouchableOpacity
-      style = {styles.StationBTN}
-      onPress={() => {
-        props.navigation.navigate("StationInfo")
-      }}>
-      <Text style = {styles.StationNameText}>Station 1</Text>
-      <Text style = {styles.StationaddressText}>충청남도 아산시 탕정면 선문로221번길 70</Text>
-    </TouchableOpacity>
+  useEffect(() => {
+    const fetchStation = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'Station'));
+        const stationData = querySnapshot.docs.map((doc) => doc.data());
+        setStation(stationData);
+      } catch (error) {
+        console.log('데이터 가져오기 실패:', error);
+      }
+    };
 
-    <TouchableOpacity
-      style = {styles.StationBTN2}
-      onPress={() => {
-        props.navigation.navigate("StationInfo")
-      }}>
-      <Text style = {styles.StationNameText2}>Station 2</Text>
-      <Text style = {styles.StationaddressText2}>충청남도 아산시 배방읍 희망로 100</Text>
-    </TouchableOpacity>
+    fetchStation();
+  }, []);
 
+  const handleStationPress = (station) => {
+    props.navigation.navigate('StationInfo', { station });
+  };
 
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {stations.map((station) => (
+          <TouchableOpacity
+            key={station.st_id}
+            onPress={() => handleStationPress(station)}
+          >
+            <Text>{station.st_id}</Text>
+            <Text>{station.um_check}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
-    </ImageBackground>
-  )
-}
+  );
+};
+
 export default Station;
--- Screens/StationScreens/StationInfo.js
+++ Screens/StationScreens/StationInfo.js
@@ -1,21 +1,29 @@
-import { useState } from "react";
-import { Text, View, TextInput, TouchableOpacity, ImageBackground } from "react-native";
-import { styles } from "../../style";
-import HorizonLine from "../../utils/HorizontalLine";
-//C:\FinalProject\node_modules\react-native\ReactAndroid\build.gradle
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
+// 우산 대여 여부 수정 필요!!!!!!
 
-/* Station 정보 관리 - 대여가능여부, 위치 및 주소, 대여 우산 여부, 폐우산 적재 갯수 확인 가능 및 수정, 삭제, 등록 */
-const Station = (props) => {
-  return(
-    <ImageBackground> 
-    <View style = {styles.mainView}>
+/* User 페이지에서 선택한 사용자 정보 열람 페이지 */
+const StationInfo = ({ route }) => {
+  const { station } = route.params;
 
-      <Text style = {styles.StationNameText}>Station 1</Text>
-      <HorizonLine/>
+  //사용자 정보 출력
+  return (
+    <View>
+      <Text>{station.st_id}</Text>
+      <Text>주소: {station.st_address}</Text>
+      <Text>폐우산 수거 상태: {station.um_check}</Text>
+      {/* state에 따라 활성화, 비활성화 */}
 
+      {/* 지도 출력 */}
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate("StationMap")
+        }}>
+        <Text>Station 위치 확인</Text>
+      </TouchableOpacity>
     </View>
-    </ImageBackground>
-  )
-}
-export default Station;
+  );
+};
+
+export default StationInfo;
