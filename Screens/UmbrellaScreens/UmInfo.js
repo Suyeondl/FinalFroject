11f606fbda01a11f93f4ea61004773cc7b47ffa8
Suyeondl <suyeon2355@gmail.com>
2023-06-06 20:21:54 +0900

    폐우산 이미지 추가

--- Screens/UmbrellaScreens/UmInfo.js
+++ Screens/UmbrellaScreens/UmInfo.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, Text, Image, ImageBackground } from 'react-native';
-import { styles } from '../../style';
+import { View, Text, Image, ImageBackground, StyleSheet } from 'react-native';
 
-/* Umbrella 페이지에서 선택한 폐우산 정보 열람 페이지 */
+/* Umbrella 스크린에서 선택한 폐우산 정보 열람 스크린 */
+
 const UmInfo = ({ route }) => {
   const { donateUm } = route.params;
   console.log(donateUm.d_num)
@@ -10,16 +10,49 @@ const UmInfo = ({ route }) => {
 
   // 폐우산 정보 출력
   return (
-    <ImageBackground style={styles.image} source={require('../../images/MainScreen.png')} resizeMode="cover">
-      <View>
-        <Text>{donateUm.d_date}</Text>
-        <Text>{donateUm.d_num}</Text>
-        <Image source={{ uri: donateUm.d_image }} style={{ width: 200, height: 200 }} />
-        <Text>{donateUm.st_id}</Text>
-        <Text>{donateUm.u_id}</Text>
+        <View style={styles.container}>
+        <Text style={styles.label}>기부 날짜</Text>
+        <Text style={styles.value}>{donateUm.d_date}</Text>
+
+        <Text style={styles.label}>우산 사진</Text>
+        <Image source={{ uri: donateUm.d_image }} style={{ width: 300, height: 300 }} />
+        
+        <Text style={styles.label}>Station ID</Text>
+        <Text style={styles.value}>{donateUm.st_id}</Text>
+
+        <Text style={styles.label}>기부한 사용자</Text>
+        <Text style={styles.value}>{donateUm.u_id}</Text>
       </View>
-    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+    // alignItems:'center',
+    // justifyContent:'center',
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'center'
+    // marginBottom: 10,
+  },
+  label: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: 10,
+    color: 'black',
+  },
+  value: {
+    fontSize: 20,
+    marginTop: 5,
+    backgroundColor: '#D9E5FF',
+    padding: 10,
+    width: '95%'
+  },
+})
 
 export default UmInfo;
